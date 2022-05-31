f8cf49d891e4ce3b80a40db3e6dc675e6059f510
Chris Miles <cmiles@obm.com>
2022-05-31 09:05:56 -0400

    added navbar

--- test/controllers/chatroom_controller_test.rb
+++ test/controllers/chatroom_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ChatroomControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get chatroom_index_url
     assert_response :success
   end
-
 end
