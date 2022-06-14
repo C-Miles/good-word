def23266228c6364f21529f98184078cf4bf3154
Chris Miles <cmiles@obm.com>
2022-06-14 09:23:18 -0400

    Add messages from UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,20 @@ class SessionsController < ApplicationController
 
   def new
   end
-
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = 'You have successfully loggin in'
+      flash[:success] = "You have successfully logged in"
       redirect_to root_path
     else
-      flash.now[:error] = 'There was something wrong with your login information'
+      flash.now[:error] = "There was something wrong with your login information"
       render 'new'
     end
   end
-
   def destroy
     session[:user_id] = nil
-    flash[:success] = 'You have successfully logged out'
+    flash[:success] = "You have successfully logged out"
     redirect_to login_path
   end
 
@@ -28,7 +26,7 @@ class SessionsController < ApplicationController
 
   def logged_in_redirect
     if logged_in?
-      flash[:error] = 'You are already logged in'
+      flash[:error] = "You are already logged in"
       redirect_to root_path
     end
   end
